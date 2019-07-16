89008356e37ce4b33e508c039dfe2b4f89ee0eab
ALVARLEUNG <alvar.leung@qq.com>
2019-07-16 22:02:26 +0800

    feat: add function for find employee by id

--- src/main/java/com/tw/apistackbase/controller/EmployeeController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.tw.apistackbase.model.Employee;
 import com.tw.apistackbase.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-//    GET       /employees    #obtain employee list
+    //    GET       /employees    #obtain employee list
 //    GET       /employees/1  # obtain a certain specific employee
 //    GET       /employees?page=1&pageSize=5  #Page query, page equals 1, pageSize equals 5
 //    GET       /employees?gender=male   #screen all male employees
@@ -24,8 +25,13 @@ public class EmployeeController {
 //    PUT       /employees/1  #update an employee
 //    DELETE    /employees/1  #delete an employee
     @GetMapping
-    public List<Employee> getAllEmployee () {
+    public List<Employee> getAllEmployee() {
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeByID(@PathVariable String id) {
+        return employeeRepository.findById(id);
     }
 
 }
